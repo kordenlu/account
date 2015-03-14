31a76e850469df1e749b24b34a1670acf54db0c1
jimingmin <jimingmin@gmail.com>
2015-03-15 02:04:49 +0800

    完善注册逻辑
    增加对注册手机号码次数的判断

--- logic/registaccount_handler.h
+++ logic/registaccount_handler.h
@@ -8,14 +8,41 @@
 #ifndef REGISTACCOUNT_HANDLER_H_
 #define REGISTACCOUNT_HANDLER_H_
 
-#include "../../include/control_head.h"
+#include "../../common/common_object.h"
 #include "../../frame/frame_impl.h"
+#include "../../frame/redis_session.h"
+#include "../../include/control_head.h"
+#include "../../include/account_msg.h"
+#include "../../include/msg_head.h"
+#include <string>
+
+using namespace std;
 using namespace FRAME;
 
-class CRegistAccountHandler : public IRedisReplyHandler
+class CRegistAccountHandler : public CBaseObject
 {
+	struct GetRegistSession
+	{
+		ControlHead			m_stCtlHead;
+		MsgHeadCS			m_stMsgHeadCS;
+		CAuthRegistPhoneReq	m_stAuthRegistPhoneReq;
+	};
 public:
-	virtual int32_t OnRedisReply(int32_t nResult, void *pReply, CBaseObject *pParam);
+
+	virtual int32_t Init()
+	{
+		return 0;
+	}
+	virtual int32_t Uninit()
+	{
+		return 0;
+	}
+	virtual int32_t GetSize()
+	{
+		return 0;
+	}
+
+	virtual int32_t OnRegistPhoneInfo(int32_t nResult, void *pReply, void *pSession);
 
 	int32_t GetAuthCodeByPhone(ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, uint8_t *pBuf, int32_t nBufSize);
 };
