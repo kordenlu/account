b695043aab60c39dee3955933ece51b7f765bce0
jimingmin <jimingmin@gmail.com>
2015-03-17 01:19:43 +0800

    增加若干消息注册

--- regist_message.h
+++ regist_message.h
@@ -12,12 +12,16 @@
 #include "../include/msg_head.h"
 #include "../include/control_head.h"
 #include "../include/account_msg.h"
-#include "logic/registaccount_handler.h"
+#include "logic/requestauth_handler.h"
+#include "logic/verifyauthcode_handler.h"
+#include "logic/registbaseinfo_handler.h"
 
 using namespace FRAME;
 
 MSGMAP_BEGIN(msgmap)
-ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_AUTHREGISTPHONE_REQ, ControlHead, MsgHeadCS, CAuthRegistPhoneReq, CRegistAccountHandler, CRegistAccountHandler::GetAuthCodeByPhone);
+ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_REQUESTAUTH_REQ, ControlHead, MsgHeadCS, CRequestAuthReq, CRequstAuthHandler, CRequstAuthHandler::GetAuthCodeByPhone);
+ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_VERIFYAUTHCODE_REQ, ControlHead, MsgHeadCS, CVerifyAuthCodeReq, CVerifyAuthCodeHandler, CVerifyAuthCodeHandler::VerifyAuthCode);
+ON_PROC_PCH_PMH_PMB_PU8_I32(MSGID_REGISTBASEINFO_REQ, ControlHead, MsgHeadCS, CRegistBaseInfoReq, CRegistBaseInfoHandler, CRegistBaseInfoHandler::RegistBaseInfo);
 MSGMAP_END(msgmap)
 
 #endif /* REGIST_MESSAGE_H_ */
