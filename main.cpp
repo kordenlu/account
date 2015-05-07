718d12ecae0f4698ea651b5ea6713dde6fb60917
jimingmin <jimingmin@gmail.com>
2015-05-08 01:03:43 +0800

    修改接口适应框架新接口

--- main.cpp
+++ main.cpp
@@ -9,15 +9,10 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdlib.h>
-#include "../netevent/net_namespace.h"
-#include "../netevent/net_impl.h"
-#include "../netevent/net_typedef.h"
-#include "../netevent/net_reactor.h"
-#include "../netevent/net_handler.h"
-#include "../netevent/net_acceptor.h"
-#include "../netevent/net_connector.h"
 #include "../logger/logger.h"
+#include "../frame/frame_impl.h"
 #include "../frame/frame.h"
+#include "../frame/cmd_thread.h"
 #include "../include/cachekey_define.h"
 #include "regist_message.h"
 #include "dispatch/msgparser_factory.h"
@@ -28,38 +23,29 @@
 #include "server_typedef.h"
 
 using namespace LOGGER;
-using namespace NETEVENT;
 using namespace FRAME;
 
 //注册到配置管理器
 REGIST_CONFIG(REGIST_PHONEINFO, RegistPhoneInfo)
 REGIST_CONFIG(REGIST_ADDRINFO, RegistAddrInfo)
 REGIST_CONFIG(USER_BASEINFO, UserBaseInfo)
+REGIST_CONFIG(USER_SESSIONINFO, UserSessionInfo)
 REGIST_CONFIG(ACCOUNT_INFO, AccountInfo)
 REGIST_CONFIG(USER_UNREADMSGLIST, UserUnreadMsgList)
 
-int32_t InitNetAndTimer(CNetHandler *pNetHandler)
+class Initer : public IInitFrame
 {
-	CServerConfig *pServerConfig = (CServerConfig *)g_Frame.GetConfig(CONFIG_SERVER);
-	if(pServerConfig == NULL)
+public:
+	virtual int32_t InitFrame(CNetHandler *pNetHandler)
 	{
-		return 1;
+		CCheckConnHandler *pCheckConnHandler = new CCheckConnHandler();
+		int32_t nTimerIndex = 0;
+		g_Frame.CreateTimer(static_cast<TimerProc>(&CCheckConnHandler::CheckConnStatus),
+				pCheckConnHandler, NULL, 10 * MS_PER_SECOND, true, nTimerIndex);
+
+		return 0;
 	}
-
-	//创建包解析器工厂
-	IPacketParserFactory *pMsgParserFactory = new CMsgParserFactory();
-	CMsgHandler *pMsgHandler = new CMsgHandler();
-
-	CAcceptor *pMsgAcceptor = new CAcceptor(pNetHandler, pMsgParserFactory, pMsgHandler);
-	pMsgAcceptor->Bind(pServerConfig->GetOutsideListenPort());
-
-	CCheckConnHandler *pCheckConnHandler = new CCheckConnHandler();
-	int32_t nTimerIndex = 0;
-	g_Frame.CreateTimer(static_cast<TimerProc>(&CCheckConnHandler::CheckConnStatus),
-			pCheckConnHandler, NULL, 10 * MS_PER_SECOND, true, nTimerIndex);
-
-	return 0;
-}
+};
 
 int32_t main()
 {
@@ -68,25 +54,34 @@ int32_t main()
 	//启动日志线程
 	CLogger::Start();
 
-	//创建网络事件处理器
-	CNetHandler *pNetHandler = new CNetHandler();
-	pNetHandler->CreateReactor();
-
-	g_Frame.AddRunner(pNetHandler);
-
-	if(g_Frame.Init(SERVER_NAME) != 0)
-	{
-		return 0;
-	}
-
-	InitNetAndTimer(pNetHandler);
+//	CCmdThread *pCmdThread = new CCmdThread(1, 1, "127.0.0.1", 5178);
+//	pCmdThread->Start();
+	g_Frame.Start(SERVER_NAME, 1, 1, "127.0.0.1", 5178, new Initer());
 
 	while(true)
 	{
-		g_Frame.Run();
+		Delay(50);
 	}
 
-	g_Frame.Uninit();
+//	//创建网络事件处理器
+//	CNetHandler *pNetHandler = new CNetHandler();
+//	pNetHandler->CreateReactor();
+//
+//	g_Frame.AddRunner(pNetHandler);
+//
+//	if(g_Frame.Init(SERVER_NAME) != 0)
+//	{
+//		return 0;
+//	}
+//
+//	InitNetAndTimer(pNetHandler);
+//
+//	while(true)
+//	{
+//		g_Frame.Run();
+//	}
+//
+//	g_Frame.Uninit();
 
 	return 0;
 }
