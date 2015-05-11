d683bab8743720d04b342530220b08ca11b0a32d
jimingmin <jimingmin@gmail.com>
2015-05-11 09:12:33 +0800

    更新设备token

---
+++ logic/updatedevicetoken_handler.cpp
@@ -0,0 +1,55 @@
+/*
+ * updatedevicetoken_handler.cpp
+ *
+ *  Created on: May 9, 2015
+ *      Author: jimm
+ */
+
+#include "updatedevicetoken_handler.h"
+#include "../../common/common_datetime.h"
+#include "../../common/common_api.h"
+#include "../../frame/frame.h"
+#include "../../frame/server_helper.h"
+#include "../../frame/redissession_bank.h"
+#include "../../logger/logger.h"
+#include "../../include/cachekey_define.h"
+#include "../../include/control_head.h"
+#include "../../include/typedef.h"
+#include "../config/string_config.h"
+#include "../server_typedef.h"
+#include "../bank/redis_bank.h"
+
+using namespace LOGGER;
+using namespace FRAME;
+
+int32_t CUpdateDeviceTokenHandler::UpdateDeviceToken(ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, uint8_t *pBuf, int32_t nBufSize)
+{
+	ControlHead *pControlHead = dynamic_cast<ControlHead *>(pCtlHead);
+	if(pControlHead == NULL)
+	{
+		return 0;
+	}
+
+	MsgHeadCS *pMsgHeadCS = dynamic_cast<MsgHeadCS *>(pMsgHead);
+	if(pMsgHeadCS == NULL)
+	{
+		return 0;
+	}
+
+	CUpdateDeviceTokenReq *pUpdateDeviceTokenReq = dynamic_cast<CUpdateDeviceTokenReq *>(pMsgBody);
+	if(pUpdateDeviceTokenReq == NULL)
+	{
+		return 0;
+	}
+
+	UserBaseInfo *pConfigUserBaseInfo = (UserBaseInfo *)g_Frame.GetConfig(USER_BASEINFO);
+
+	CRedisBank *pRedisBank = (CRedisBank *)g_Frame.GetBank(BANK_REDIS);
+	CRedisChannel *pUserBaseInfoChannel = pRedisBank->GetRedisChannel(pConfigUserBaseInfo->string);
+	pUserBaseInfoChannel->HMSet(NULL, itoa(pMsgHeadCS->m_nSrcUin), "%s %s", pConfigUserBaseInfo->devicetoken, pUpdateDeviceTokenReq->m_strDeviceToken.c_str());
+
+	return 0;
+}
+
+
+
---
+++ logic/updatedevicetoken_handler.h
@@ -0,0 +1,42 @@
+/*
+ * updatedevicetoken_handler.h
+ *
+ *  Created on: May 9, 2015
+ *      Author: jimm
+ */
+
+#ifndef UPDATEDEVICETOKEN_HANDLER_H_
+#define UPDATEDEVICETOKEN_HANDLER_H_
+
+#include "../../common/common_object.h"
+#include "../../frame/frame_impl.h"
+#include "../../frame/redis_session.h"
+#include "../../include/control_head.h"
+#include "../../include/account_msg.h"
+#include "../../include/msg_head.h"
+#include <string>
+
+using namespace std;
+using namespace FRAME;
+
+class CUpdateDeviceTokenHandler : public CBaseObject
+{
+public:
+
+	virtual int32_t Init()
+	{
+		return 0;
+	}
+	virtual int32_t Uninit()
+	{
+		return 0;
+	}
+	virtual int32_t GetSize()
+	{
+		return 0;
+	}
+
+	int32_t UpdateDeviceToken(ICtlHead *pCtlHead, IMsgHead *pMsgHead, IMsgBody *pMsgBody, uint8_t *pBuf, int32_t nBufSize);
+};
+
+#endif /* UPDATEDEVICETOKEN_HANDLER_H_ */
